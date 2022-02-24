605a7aa1cd0b73a5a4cfabb01016d62982168457
mrdmitrievv <mrdmitrievv@yandex.ru>
2022-02-24 17:46:58 +0300

    Initial Commit

---
+++ TrafficLights/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TrafficLights
+//
+//  Created by Артём Дмитриев on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        
+    }
+
+
+}
+
